d37b40483fbb611c7cc4435d3b82dd75d57b5e8c
ziadm101 <ziadmabrouk1@gmail.com>
2024-12-26 14:22:33 -0500

    added full sprites and 8 step animations

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -7,7 +7,13 @@ import java.awt.image.BufferedImage;
 public class Entity {
     public int x, y;
     public int speed;
-    public BufferedImage down1, down2, up1, up2, left1, left2, right1, right2;
+    public BufferedImage down1, down2, down3, down4, down5, down6, down7, down8, down9;
+
+    public BufferedImage up1, up2, up3, up4, up5, up6, up7, up8, up9;
+
+    public BufferedImage left1, left2, left3, left4, left5, left6, left7, left8, left9;
+
+    public BufferedImage right1, right2, right3, right4, right5, right6, right7, right8, right9;
     public String direction = "down";
     public int spriteCounter =0;
     public int spriteNumber =0;
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -25,95 +25,247 @@ public class Player extends Entity {
         y = 100;
 
         //Default speed
-        speed = 4;
+        speed = 3;
         direction = "resting";
     }
 
     public void update() { // Update player position
-        if (keyHandler.upPressed) {
-            direction = "up";
-            if (y - speed >= 0) { // Prevent moving above the top border
-                y -= speed;
-            }
-        } else if (keyHandler.downPressed) {
-            direction = "down";
-            if (y + speed + gamePanel.tileSize <= gamePanel.screenHeight) { // Prevent moving below the bottom border
-                y += speed;
-            }
-        } else if (keyHandler.leftPressed) {
-            direction = "left";
-            if (x - speed >= 0) { // Prevent moving left past the left border
-                x -= speed;
-            }
-        } else if (keyHandler.rightPressed) {
-            direction = "right";
-            if (x + speed + gamePanel.tileSize <= gamePanel.screenWidth) { // Prevent moving right past the right border
-                x += speed;
-            }
-        }
-        spriteCounter++;
-        if (spriteCounter > 20) {
-            spriteNumber = (spriteNumber + 1) % 2;
-            spriteCounter = 0;
-        }
-    }
-    public void draw(Graphics2D  g2) {
-        BufferedImage image = null;
 
-        if (direction.equals("up")) {
-            if (spriteNumber == 0) {
-                image = up1;
+        if (keyHandler.downPressed || keyHandler.leftPressed || keyHandler.rightPressed || keyHandler.upPressed) {
+            if (keyHandler.upPressed) {
+                direction = "up";
+                if (y - speed >= 0) { // Prevent moving above the top border
+                    y -= speed;
+                }
+            } else if (keyHandler.downPressed) {
+                direction = "down";
+                if (y + speed + gamePanel.tileSize <= gamePanel.screenHeight) { // Prevent moving below the bottom border
+                    y += speed;
+                }
+            } else if (keyHandler.leftPressed) {
+                direction = "left";
+                if (x - speed >= 0) { // Prevent moving left past the left border
+                    x -= speed;
+                }
+            } else {
+                direction = "right";
+                if (x + speed + gamePanel.tileSize <= gamePanel.screenWidth) { // Prevent moving right past the right border
+                    x += speed;
+                }
             }
-            else {
-                image = up2;
-
-            }
-        }
-
-        else if (direction.equals("right")) {
-            if (spriteNumber == 0) {
-                image = right1;
-            }
-            else {
-                image = right2;
-
-            }
-        }
-
-        else if (direction.equals("left")) {
-            if (spriteNumber == 0) {
-                image = left1;
-            }
-            else {
-                image = left2;
-
+            spriteCounter++;
+            if (spriteCounter > 7) {
+                spriteNumber = (spriteNumber + 1) % 9;
+                spriteCounter = 0;
             }
         }
 
         else {
+            spriteNumber = -1;
+        }
+
+    }
+    public void draw(Graphics2D  g2) {
+        BufferedImage image = null;
+
+
+
+        if (direction.equals("up")) {
+
+            if (spriteNumber == -1) {
+                image = up1;
+            }
             if (spriteNumber == 0) {
+                image = up6;
+            }
+            else if (spriteNumber == 1) {
+                image = up2;
+
+            }
+
+            else if (spriteNumber == 2) {
+                image = up3;
+            }
+
+            else if (spriteNumber == 3) {
+                image = up4;
+            }
+
+            else if (spriteNumber == 4) {
+                image = up5;
+            }
+
+            else if (spriteNumber == 5) {
+                image = up6;
+            }
+
+            else if (spriteNumber == 6) {
+                image = up7;
+            }
+
+            else if (spriteNumber == 7) {
+                image = up8;
+            }
+
+            else if (spriteNumber == 8) {
+                image = up9;
+            }
+        }
+
+        else if (direction.equals("right")) {
+            if (spriteNumber == -1) {
+                image = right1;
+            }
+            else if (spriteNumber == 0) {
+                image = right1;
+
+            }
+            else if (spriteNumber == 1) {
+                image = right2;
+            }
+            else if (spriteNumber == 2) {
+                image = right3;
+            }
+            else if (spriteNumber == 3) {
+                image = right4;
+            }
+            else if (spriteNumber == 4) {
+                image = right5;
+            }
+            else if (spriteNumber == 5) {
+                image = right6;
+            }
+            else if (spriteNumber == 6) {
+                image = right7;
+            }
+            else if (spriteNumber == 7) {
+                image = right8;
+
+            }
+            else if (spriteNumber == 8) {
+                image = right9;
+            }
+        }
+
+        else if (direction.equals("left")) {
+            if (spriteNumber == -1) {
+                image = left1;
+            }
+            else if (spriteNumber == 0) {
+                image = left1;
+
+            }
+
+            else if (spriteNumber == 1) {
+                image = left2;
+            }
+            else if (spriteNumber == 2) {
+                image = left3;
+            }
+            else if (spriteNumber == 3) {
+                image = left4;
+            }
+            else if (spriteNumber == 4) {
+                image = left5;
+            }
+            else if (spriteNumber == 5) {
+                image = left6;
+            }
+            else if (spriteNumber == 6) {
+                image = left7;
+            }
+            else if (spriteNumber == 7) {
+                image = left8;
+            }
+            else if (spriteNumber == 8) {
+                image = left9;
+            }
+        }
+
+        else  {
+            if (spriteNumber == -1) {
                 image = down1;
             }
-            else {
+            else if (spriteNumber == 0) {
+                image = down6;
+
+            }
+            else if (spriteNumber == 1) {
                 image = down2;
 
+            }
+
+            else if (spriteNumber == 2) {
+                image = down3;
+            }
+
+            else if (spriteNumber == 3) {
+                image = down4;
+            }
+
+            else if (spriteNumber == 4) {
+                image = down5;
+            }
+
+            else if (spriteNumber == 5) {
+                image = down6;
+            }
+
+            else if (spriteNumber == 6) {
+                image = down7;
+            }
+
+            else if (spriteNumber == 7) {
+                image = down8;
+            }
+
+            else if (spriteNumber == 8) {
+                image = down9;
             }
         }
 
 
-        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+        g2.drawImage(image, x, y, gamePanel.tileSize * 2, gamePanel.tileSize * 2, null);
     }
 
     public void getPlayerImage() {
         try {
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/playerResting1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/playerResting2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown2.png"));
+            down3 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown3.png"));
+            down4 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown4.png"));
+            down5 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown5.png"));
+            down6 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown6.png"));
+            down7 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown7.png"));
+            down8 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown8.png"));
+            down9 = ImageIO.read(getClass().getResourceAsStream("/player/playerDown9.png"));
             up1 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp1.png"));
             up2 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp2.png"));
+            up3 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp3.png"));
+            up4 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp4.png"));
+            up5 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp5.png"));
+            up6 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp6.png"));
+            up7 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp7.png"));
+            up8 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp8.png"));
+            up9 = ImageIO.read(getClass().getResourceAsStream("/player/playerUp9.png"));
             left1 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft1.png"));
             left2 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft2.png"));
+            left3 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft3.png"));
+            left4 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft4.png"));
+            left5 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft5.png"));
+            left6 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft6.png"));
+            left7 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft7.png"));
+            left8 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft8.png"));
+            left9 = ImageIO.read(getClass().getResourceAsStream("/player/playerLeft9.png"));
             right1 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight1.png"));
             right2 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight2.png"));
+            right3 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight3.png"));
+            right4 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight4.png"));
+            right5 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight5.png"));
+            right6 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight6.png"));
+            right7 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight7.png"));
+            right8 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight8.png"));
+            right9 = ImageIO.read(getClass().getResourceAsStream("/player/playerRight9.png"));
         } catch (IOException e) {
             e.printStackTrace();
 
